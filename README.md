0c50e09563781297b7d343d80601b959682ad63c
PAULO HENRIQUE MARIOTTO NAHES <paulo.nahes@buscapecompany.com>
2018-09-24 18:56:45 -0300

    Adding scripts

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # zabbix-deregister-lambda-on-ec2-terminatie
 Deregister Ec2 Instance from Zabbix API when EC2 Terminate
+
+## CloudWatch Event
+
+Use:
+
+* Service Name: EC2
+* Event Type: EC@ Instance State-change Notification
+* Specific state(s): terminated
+* Any instance
+
+* Event Pattern Preview
+
+```
+{
+  "source": [
+    "aws.ec2"
+  ],
+  "detail-type": [
+    "EC2 Instance State-change Notification"
+  ],
+  "detail": {
+    "state": [
+      "terminated"
+    ]
+  }
+}
+```
+
+## Lambda Function
+
+Import lambda function and Library, changing the vars bellow:
+
+
+```
+server =
+username = 
+password =
+```
+
+### Extras
+
+Variable such as: 
+```st_disabled``` and ```st_enabled``` define how script will work, enabling or disabling hosts on Zabbix Server. 
+
+To work fine, change "state" on "Specific State" to running.
---
+++ zabbix_request.py
@@ -0,0 +1,50 @@
+from pyzabbix import ZabbixAPI, ZabbixAPIException
+import boto3
+import sys
+import json
+
+server = 'http://xxx.yyy.zzz.aaa'
+username = 'zabbix_api_user'
+password = 'password_password_password'
+
+st_disabled = 1
+st_enabled = 0
+status = st_disabled
+    
+
+def lambda_handler(event, context):
+    #print json.dumps(event)
+    
+    thisInstanceID = event['detail']['instance-id']
+
+    ec2 = boto3.resource('ec2')
+    ec2instance = ec2.Instance(thisInstanceID)
+    instancename = ''
+    for tags in ec2instance.tags:
+        if tags["Key"] == 'Name':
+            instancename = tags["Value"]
+            #print("instance-id: " + thisInstanceID + " Instance-name: " +  instancename )
+            
+    zapi = ZabbixAPI(server, user=username, password=password)
+    host_name = instancename
+    #print "host_name " + host_name
+    hosts = zapi.host.get(filter={"host": host_name})
+    #print "hosts " + str(hosts)
+    if hosts:
+        host_id = hosts[0]["hostid"]
+        #print("Found host id {0}".format(host_id))
+    
+        try:
+            item = zapi.host.update(
+                hostid=host_id,
+                status=status
+            )
+        except ZabbixAPIException as e:
+            print(e)
+            sys.exit()
+        print("Status Changed to: {0} - Host: {1} {2}".format(status, host_id, host_name))
+    else:
+        print("Host {0} not found on zabbix - Ignoring".format(host_name))
+
+
+
